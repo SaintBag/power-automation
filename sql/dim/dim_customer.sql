a056e6534121475c770b835a287eafa243b3d063
Sebastian Worek <sebastian@Sebastians-MacBook-Air.local>
2025-12-16 19:41:15 +0100

    Add dim_customer view based on semantic model

--- sql/dim/dim_customer.sql
+++ sql/dim/dim_customer.sql
@@ -1,10 +1,8 @@
--- Dimension: Customer
--- Grain: 1 row per CustomerID
-
 CREATE OR REPLACE VIEW dim_customer AS
 SELECT DISTINCT
-    CustomerID      AS customer_key,
-    CustomerName    AS customer_name,
-    Region           AS region
-FROM sales_data
-WHERE CustomerID IS NOT NULL;
+    sd.CustomerID   AS customer_id,
+    sd.CustomerName AS customer_name,
+    sd.Region       AS region
+FROM sales_data sd
+WHERE sd.CustomerID IS NOT NULL;
+
