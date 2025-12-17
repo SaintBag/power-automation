c12783fa4b3d95dfb960016787ada7ba00806ca3
Sebastian Worek <sebastian@Sebastians-MacBook-Air.local>
2025-12-17 19:26:08 +0100

    Revert "Merge pull request #18 from SaintBag/test/negative-missing-sql"
    This reverts commit 1832f79847ed232cb3096c24dbb7b02d9afdeb3f, reversing
    changes made to efea04f6b80ecc13561fb0bd992b0fc7b2337260.

---
+++ sql/fact/fact_sales.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW fact_sales AS
+SELECT
+    sd.OrderID      AS order_id,
+
+    -- Foreign keys
+    sd.CustomerID   AS customer_id,
+    sd.ProductID    AS product_id,
+    sd.OrderDate    AS order_date,
+
+    -- Measures (AGGREGATED)
+    SUM(sd.Quantity)      AS quantity,
+    SUM(sd.TotalAmount)   AS total_amount
+FROM sales_data sd
+GROUP BY
+    sd.OrderID,
+    sd.CustomerID,
+    sd.ProductID,
+
